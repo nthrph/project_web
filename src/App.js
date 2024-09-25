4d92766506544316fa0af6ed6d1695034e470e49
nithaaa <nitharee2@gmail.com>
2024-09-25 22:23:51 +0700

    Add file แก้เยอะมากกกกก

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './page/shop/NavBar_component';
+import Status from './page/shop/Status';  // นำเข้าหน้าของ Status
+import CakeList from './page/shop/Cake';
+import CookieList from './page/shop/Cookie';
+import DrinkList from './page/shop/Drink';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />  {/* แสดง Navbar ที่ทุกหน้าจะมี */}
+      <Routes>
+        <Route path="/shop/Cake" element={<CakeList />} />  {/* กำหนดเส้นทางไปที่ Cake */}
+        <Route path="/shop/Cookie" element={<CookieList />} />  {/* กำหนดเส้นทางไปที่ Cookie */}
+        <Route path="/shop/Drink" element={<DrinkList />} />  {/* กำหนดเส้นทางไปที่ Drink */}
+        <Route path="/shop/Status" element={<Status />} />  {/* กำหนดเส้นทางไปที่ Status */}
+      </Routes>
+    </Router>
   );
 }
 
